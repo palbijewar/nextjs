55d9556acd90d0c0a0502e9da3e59303117f82a9
unknown <pal.bijewar@bhavithatech.com>
2025-01-28 17:22:43 +0530

    added: nested dynamic routing

---
+++ src/app/products/[productId]/reviews/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductDetails() {
+    return(
+        <>
+         <h1>Review list:</h1>
+         <h2>Review 1</h2>
+         <h2>Review 2</h2>
+         <h2>Review 3</h2>
+         </>
+         )
+}
